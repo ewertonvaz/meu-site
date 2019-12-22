e1555d833c8bb14c82c1d1848d741be21e70863c
ewertonvaz <ewerton.vaz@gmail.com>
2019-12-22 15:36:17 -0300

    Layouts criados

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.principal')
+@extends('layouts.main')
 
 @section('content')
   <h2>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,14 @@
-@extends('layouts.principal')
+@extends('layouts.main')
+
+@section('titulo', 'Página principal')
 
 @section('content')
     <h2>
         Minha página inicial
     </h2>
+@endsection
+
+@section('lead-text')
+    Use este documento como um ponto de partida para iniciar de forma rápida um novo projeto.<br>
+     Tudo que você tem é este texto e um esqueleto de documento HTML.
 @endsection
